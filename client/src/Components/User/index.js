34e8ac09eb3663f6cf6ba812e1ea1f178eabdadd
gokhan.maden <gokhan.maden@bilyoner.com>
2018-10-23 17:32:23 +0300

    Header setup part1

--- client/src/Components/User/index.js
+++ client/src/Components/User/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import UserLayout from '../../hoc/user';
 import Button from '../Utils/Button';
 
-const UserDashboard = () => {
+const UserDashboard = ({user}) => {
+
+  if(user && user.userData) {
+    var userName = user.userData.name;
+    var userLastname = user.userData.lastname;
+    var userEmail = user.userData.email;
+  }
   return (
     <UserLayout>
       <div>
         <div className="user_nfo_panel">
           <h1>User Information</h1>
           <div>
-            <span>Name</span>
-            <span>Lastname</span>
-            <span>Email</span>
+            <span>Name: {userName}</span>
+            <span>Lastname: {userLastname}</span>
+            <span>Email: {userEmail}</span>
           </div>
           <Button 
             type="default"
